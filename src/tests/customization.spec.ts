41525e5e4706bd5ba35557b02de3fb88b0499d56
Ni55aN <huccah@gmail.com>
2023-04-21 22:20:30 +0300

    test: add customization tests and snapshots

---
+++ src/tests/customization.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+import { getGraphView, takeBeforeEach } from './helper'
+
+const { getContainer } = takeBeforeEach('?template=customization', 1000, 500)
+
+test('customization: has nodes', async ({}) => {
+  const { nodes, connections } = await getGraphView(getContainer())
+
+  expect(await nodes()).toHaveLength(2)
+  expect(await connections()).toHaveLength(1)
+})
+
+test('customization: snapshot', async ({ page }) => {
+  await page.waitForTimeout(500)
+
+  expect(await page.screenshot()).toMatchSnapshot('customization.png')
+})
