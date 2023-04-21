965e58d482d2d4e7ee2a432f84716274a862d81f
Ni55aN <huccah@gmail.com>
2023-04-21 21:56:05 +0300

    test: update Perf tests

--- src/tests/perf.spec.ts
+++ src/tests/perf.spec.ts
@@ -1,15 +1,22 @@
 import { test, expect } from '@playwright/test'
 import { getGraphView, takeBeforeEach } from './helper'
 
-const { getContainer } = takeBeforeEach('?template=perf&rows=10&cols=10', 2000, 500)
+test.describe('Perf', () => {
+  test.beforeEach(({ browserName }) => {
+    test.skip(browserName === 'webkit' && String(process.env.APP).startsWith('angular'), 'WebKit has problems running Angular apps with a lot of elements (at least in WSL2 environment)');
+  })
+  const { getContainer } = takeBeforeEach('?template=perf&rows=10&cols=10', 500, 500)
 
-test('perf: has nodes', async ({}) => {
-  const { nodes, connections } = await getGraphView(getContainer())
+  test('has nodes', async ({ page }) => {
+    const { nodes, connections } = await getGraphView(getContainer())
 
-  expect(await nodes()).toHaveLength(100)
-  expect(await connections()).toHaveLength(99)
-})
+    await page.waitForTimeout(1000)
+    expect(await nodes()).toHaveLength(100)
+    expect(await connections()).toHaveLength(99)
+  })
 
-test('perf: snapshot', async ({ page }) => {
-  expect(await page.screenshot()).toMatchSnapshot('perf.png')
+  test('snapshot', async ({ page }) => {
+    await page.waitForTimeout(1000)
+    expect(await page.screenshot()).toMatchSnapshot('perf.png')
+  })
 })
