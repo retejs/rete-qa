4a2e284f076c13ba50360da8a3503424c9526398
Ni55aN <stoliarov.v.s@gmail.com>
2024-12-27 14:56:43 +0200

    ci: use react-vite

--- src/commands/init/index.ts
+++ src/commands/init/index.ts
@@ -2,6 +2,7 @@ import { App } from 'rete-kit'
 
 export const targets: { stack: App.AppStack, versions: number[] }[] = [
   { stack: 'react', versions: [16, 17, 18] },
+  { stack: 'react-vite', versions: [16, 17, 18] },
   { stack: 'vue', versions: [2, 3] },
   { stack: 'angular', versions: [12, 13, 14, 15, 16, 17, 18] },
   { stack: 'svelte', versions: [3, 4] },
@@ -21,7 +22,7 @@ export const fixtures = targets
 export function getFeatures({ stack, version }: Pick<(typeof fixtures)[0], 'stack' | 'version'>, next: boolean) {
   return [
     stack === 'angular' && new App.Features.Angular(version as 12 | 13 | 14 | 15 | 16 | 17 | 18, next),
-    stack === 'react' && new App.Features.React(version, stack, next),
+    ['react', 'react-vite'].includes(stack) && new App.Features.React(version, stack, next),
     stack === 'vue' && new App.Features.Vue(version as 2 | 3, next),
     stack === 'svelte' && new App.Features.Svelte(version as 3 | 4, next),
     stack === 'lit-vite' && new App.Features.Lit(version as 3, next),
